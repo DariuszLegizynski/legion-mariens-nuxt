ccab54a2baedd9626c29389f3dfbb486aaf2fcc5
Dariusz Legizynski <d.legizynski@.com>
2024-08-07 15:00:01 +0200

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "hsl(227, 46%, 44%)",
+				accent: "hsl(339, 84%, 43%)",
+				black: "hsl(210, 11%, 15%)",
+				white: "hsl(5, 0%, 100%)",
+				grey: "hsla(0, 0%, 0%, 0.15)",
+			},
+			screens: {
+				xxs: "375px",
+				xs: "425px",
+				"3xl": "1920px",
+			},
+		},
+	},
+	plugins: [],
+}
+export default config
+
