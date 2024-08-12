6ef4517b4a0f8cb595eb5336489f5b4e1a398638
Dariusz Legizynski <d.legizynski@.com>
2024-08-12 09:34:04 +0200

    fix foote logo imager warnings

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
 	return (
 		<footer className="flex flex-col items-center justify-center text-primary bg-white py-8 border-t border-accent">
 			<Link href="/" className="flex flex-col items-center justify-center">
-				<Image src="/images/Standarte_LM.svg" alt="Logo der Standarde von der Legion Mariens" width={32} height={32} />
+				<Image className="h-16 w-auto" src="/images/Standarte_LM.svg" alt="Logo der Standarde von der Legion Mariens" width={32} height={60} />
 				<span className="mt-4">© {currentYear} Legion-Mariens.at</span>
 			</Link>
 		</footer>
